ae6f0c4d0fd0b21df1cc99ca2b283324ae0bb37f
ganesh <ganeshpraba02@gmail.com>
2019-09-14 14:07:08 -0700

    new a-z button, plus color change

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.todolist;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.provider.FontsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -9,28 +16,47 @@ import android.widget.ListView;
 import android.widget.Button;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
     //ListView listview;
-    ArrayList<String> sheet = new ArrayList<>();
+    ArrayList<SpannableString> sheet = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-         final EditText item =  findViewById(R.id.editText);
-        //
-
+        final EditText item =  findViewById(R.id.editText);
+        ListView list2 = findViewById(R.id.list);
+        final ArrayAdapter<SpannableString> adapt = new ArrayAdapter<>(this,android.R.layout.select_dialog_multichoice, sheet);
+        list2.setAdapter(adapt);
         Button add2 = findViewById(R.id.button);
         add2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                SpannableString x = new SpannableString(item.getText());
+                ForegroundColorSpan textcolor = new ForegroundColorSpan(Color.WHITE);
+                StyleSpan bold = new StyleSpan(Typeface.BOLD);
 
-                sheet.add(item.getText().toString());
+                x.setSpan(textcolor,0,item.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                x.setSpan(bold,0,item.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+                sheet.add(x);
+                adapt.notifyDataSetChanged();
             }
         });
-        ListView list2 = (ListView) findViewById(R.id.list);
-        ArrayAdapter<String> adapt = new ArrayAdapter<String>(this,android.R.layout.select_dialog_multichoice, sheet);
-        list2.setAdapter(adapt);
+        Button alpha = findViewById(R.id.alpha);
+        alpha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Collections.sort(sheet);
+                adapt.notifyDataSetChanged();
+
+            }
+        });
+
+
+
     }
 }
