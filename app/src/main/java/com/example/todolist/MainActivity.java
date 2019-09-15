c090c66c3d9efdd8dff431408b687bb6e4dc09e9
ganesh <ganeshpraba02@gmail.com>
2019-09-14 19:51:18 -0700

    deleting option added

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.todolist;
 
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.provider.FontsContract;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.SpannableString;
@@ -17,6 +19,7 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -25,15 +28,17 @@ import java.util.Collections;
 public class MainActivity extends AppCompatActivity {
     //ListView listview;
     ArrayList<SpannableString> sheet = new ArrayList<>();
-    ListView list2 = findViewById(R.id.list);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final EditText item =  findViewById(R.id.editText);
+        final ListView list2 = findViewById(R.id.list);
+
 
         final ArrayAdapter<SpannableString> adapt = new ArrayAdapter<>(this,android.R.layout.simple_list_item_multiple_choice, sheet);
+        list2.setAdapter(adapt);
 
 //        Button add2 = findViewById(R.id.button);
 //        add2.setOnClickListener(new View.OnClickListener() {
@@ -70,24 +75,33 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+        list2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String d = ((TextView) view).getText().toString();
+            }
+        });
         list2.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                SparseBooleanArray postionchecker = list2.getCheckedItemPositions();
-                int count = list2.getCount();
-                for (int i = count - 1;i >= 0;i--) {
-                    if (postionchecker.get(i)) {
-                        adapt.remove(sheet.get(i));
+                final int wh_item = position;
+                new AlertDialog.Builder(MainActivity.this)
+                        .setIcon(android.R.drawable.ic_delete)
+                        .setTitle("Are you sure")
+                        .setMessage("Do you want to delete")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                sheet.remove(wh_item);
+                                adapt.notifyDataSetChanged();
+                            }
+                        })
+                        .setNegativeButton("No", null)
+                        .show();
 
-                    }
-                }
-                postionchecker.clear();
-                adapt.notifyDataSetChanged();
-
-                return false;
+                return true;
             }
         });
-        list2.setAdapter(adapt);
 
 
 //        SparseBooleanArray postionchecker = list2.getCheckedItemPositions();
