8d75be658eb32ef46d17368944dfb558cf5390f3
ganesh <ganeshpraba02@gmail.com>
2019-09-15 11:31:42 -0700

    edit text returns to blank after enter and dose not exist when  typing

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -40,25 +40,11 @@ public class MainActivity extends AppCompatActivity {
         final ArrayAdapter<SpannableString> adapt = new ArrayAdapter<>(this,android.R.layout.simple_list_item_multiple_choice, sheet);
         list2.setAdapter(adapt);
 
-//        Button add2 = findViewById(R.id.button);
-//        add2.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                SpannableString x = new SpannableString(item.getText());
-//                ForegroundColorSpan textcolor = new ForegroundColorSpan(Color.WHITE);
-//                StyleSpan bold = new StyleSpan(Typeface.BOLD);
-//
-//                x.setSpan(textcolor,0,item.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-//                x.setSpan(bold,0,item.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-//
-//                sheet.add(x);
-//                adapt.notifyDataSetChanged();
-//            }
-//        });
+
         item.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
-                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                if ((event.getAction() == KeyEvent.ACTION_UP)&&(keyCode == KeyEvent.KEYCODE_ENTER)) {
                     SpannableString x = new SpannableString(item.getText());
                     ForegroundColorSpan textColor = new ForegroundColorSpan(Color.WHITE);
                     StyleSpan bold = new StyleSpan(Typeface.BOLD);
@@ -68,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
 
                     sheet.add(0,x);
                     adapt.notifyDataSetChanged();
+                    item.getText().clear();
                     return true;
 
                 }
@@ -86,9 +73,9 @@ public class MainActivity extends AppCompatActivity {
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
                 final int wh_item = position;
                 new AlertDialog.Builder(MainActivity.this)
-                        .setIcon(android.R.drawable.ic_delete)
-                        .setTitle("Are you sure")
-                        .setMessage("Do you want to delete")
+                        .setIcon(android.R.drawable.ic_menu_delete)
+                        .setTitle("Are you sure?")
+                        .setMessage("Do you want to delete this")
                         .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
@@ -104,17 +91,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-//        SparseBooleanArray postionchecker = list2.getCheckedItemPositions();
-//        int count = list2.getCount();
-//        for (int item = count - 1;item >= 0;item--) {
-//            if (postionchecker.get(item)) {
-//                adapt.remove(sheet.get(item));
-//
-//            }
-//        }
-//        postionchecker.clear();
-//        adapt.notifyDataSetChanged();
-//        return false;
+
 
 
         Button alpha = findViewById(R.id.alpha);
