f65f1071439e418b9cffe8ad7e888c8904a36636
Ganesh Prabagaran <55032089+ganeshpraba@users.noreply.github.com>
2020-03-12 14:51:13 -0700

    adds cross out, home page, and clear all option

---
+++ app/src/main/java/com/example/todolist/HomeScreen.java
@@ -0,0 +1,28 @@
+package com.example.todolist;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeScreen extends AppCompatActivity {
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+
+        button = findViewById(R.id.toList);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMain();
+            }
+        });
+    }
+    public void openMain(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+    }
+}
--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -3,60 +3,72 @@ package com.example.todolist;
 import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.Typeface;
-import android.provider.FontsContract;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.Spanned;
+import android.text.style.AbsoluteSizeSpan;
 import android.text.style.ForegroundColorSpan;
+import android.text.style.StrikethroughSpan;
 import android.text.style.StyleSpan;
-import android.util.SparseBooleanArray;
 import android.view.KeyEvent;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.View.OnKeyListener;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Button;
-import android.widget.TextView;
-
+import android.os.Handler;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
+
 
 public class MainActivity extends AppCompatActivity {
-    //ListView listview;
     ArrayList<SpannableString> sheet = new ArrayList<>();
-
+    int num = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final EditText item =  findViewById(R.id.editText);
+        final EditText item = findViewById(R.id.editText);
         final ListView list2 = findViewById(R.id.list);
+        final ArrayAdapter<SpannableString> adapt = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, sheet);
         list2.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-        CustomAdapter customAdapter = new CustomAdapter();
-        list2.setAdapter(customAdapter);
-        final ArrayAdapter<SpannableString> adapt = new ArrayAdapter<>(this,android.R.layout.simple_list_item_multiple_choice, sheet);
         list2.setAdapter(adapt);
 
-        // Listener to see when enter is pressed
-        item.setOnKeyListener(new View.OnKeyListener() {
+        item.setOnKeyListener(new OnKeyListener() {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
-                if ((event.getAction() == KeyEvent.ACTION_UP)&&(keyCode == KeyEvent.KEYCODE_ENTER)) {
-                    SpannableString x = new SpannableString(item.getText());
+                SpannableString x;
+                if ((event.getAction() == KeyEvent.ACTION_UP) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    if (item.getText().toString().equals("ganesh is gay")) {
+                        x = new SpannableString("   Sudhanva is gay   ");
+
+                        ForegroundColorSpan textColor = new ForegroundColorSpan(Color.WHITE);
+                        StyleSpan bold = new StyleSpan(Typeface.BOLD);
+                        AbsoluteSizeSpan sizeText = new AbsoluteSizeSpan(35);
+                        x.setSpan(textColor, 0, item.length() + 8, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                        x.setSpan(bold, 0, item.length() + 8, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                        x.setSpan(sizeText, 0, item.length() + 8, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+                        sheet.add(0, x);
+
+                        adapt.notifyDataSetChanged();
+                        item.getText().clear();
+                        return true;
+                    } else {
+                        x = new SpannableString(item.getText());
+                    }
                     ForegroundColorSpan textColor = new ForegroundColorSpan(Color.WHITE);
                     StyleSpan bold = new StyleSpan(Typeface.BOLD);
+                    AbsoluteSizeSpan sizeText = new AbsoluteSizeSpan(35);
+                    x.setSpan(textColor, 0, item.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                    x.setSpan(bold, 0, item.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                    x.setSpan(sizeText, 0, item.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
 
-                    x.setSpan(textColor,0,item.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                    x.setSpan(bold,0,item.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                    sheet.add(0, x);
 
-                    sheet.add(x);
                     adapt.notifyDataSetChanged();
                     item.getText().clear();
                     return true;
@@ -66,87 +78,101 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
-        final ArrayList<SpannableString> sheet2 = new ArrayList<>();
-//        list2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                SpannableString d = new SpannableString(((TextView) view).getText());
-//                if(sheet2.contains(d)){
-//                    //sheet2.remove(d);
-//                }
-//                else{
-//                    //sheet2.add(d);
-//                }
-//            }
-//        });
-        // long click to delete
+
         list2.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
                 final int wh_item = position;
+
                 new AlertDialog.Builder(MainActivity.this)
-                        .setIcon(android.R.drawable.ic_menu_delete)
+                        .setIcon(android.R.drawable.ic_delete)
                         .setTitle("Are you sure?")
                         .setMessage("Do you want to delete this")
                         .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 sheet.remove(wh_item);
+
+
                                 adapt.notifyDataSetChanged();
                             }
                         })
                         .setNegativeButton("No", null)
                         .show();
+                adapt.notifyDataSetChanged();
 
                 return true;
             }
         });
 
+        list2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            SpannableString temp;
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                final int pos = i;
+                num++;
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (num == 1) {
+                            temp = sheet.get(pos);
+
+                            temp.setSpan(new StrikethroughSpan(), 0, sheet.get(pos).length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                            sheet.add(temp);
+                            sheet.remove(pos);
+                            adapt.notifyDataSetChanged();
+                        } else if (num == 2) {
+                            String x = sheet.get(pos).toString();
+                            SpannableString c = new SpannableString(x);
+                            ForegroundColorSpan textColor = new ForegroundColorSpan(Color.WHITE);
+                            StyleSpan bold = new StyleSpan(Typeface.BOLD);
+                            AbsoluteSizeSpan sizeText = new AbsoluteSizeSpan(25);
+                            c.setSpan(textColor, 0, c.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                            c.setSpan(bold, 0, c.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                            c.setSpan(sizeText, 0, c.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                            sheet.remove(pos);
+                            sheet.add(0, c);
 
 
+                            adapt.notifyDataSetChanged();
 
+                        }
+                        num = 0;
+
+                    }
+                }, 500);
+
+
+            }
+        });
 
         Button alpha = findViewById(R.id.alpha);
         alpha.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Collections.sort(sheet);
+                new AlertDialog.Builder(MainActivity.this)
+                        .setIcon(android.R.drawable.ic_delete)
+                        .setTitle("Are you want to CLEAR ALL?")
+                        .setMessage("Do you want to delete this")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                sheet.clear();
+
+
+                                adapt.notifyDataSetChanged();
+                            }
+                        })
+                        .setNegativeButton("No", null)
+                        .show();
                 adapt.notifyDataSetChanged();
+
 
             }
         });
 
-
-
     }
-    class CustomAdapter extends BaseAdapter{
-
-
-        @Override
-        public int getCount() {
-            return sheet.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return null;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            convertView = getLayoutInflater().inflate(R.layout.customlayout, null);
-            TextView text = (TextView)convertView.findViewById(R.id.textView_text);
-            CheckBox check = (CheckBox)convertView.findViewById(R.id.checkBox_check);
-            text.setText(sheet.get(position));
-
-            return null;
-        }
-    }
-
 
 }
